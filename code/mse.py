183937fad70914051350b6e45f4b2e89103a5d7d
mikepackard415 <72175794+mikepackard415@users.noreply.github.com>
2023-06-10 23:16:10 -0400

    Update mse.py

--- code/mse.py
+++ code/mse.py
@@ -2,15 +2,35 @@ import numpy as np
 
 # Calculate Squared Errors
 def mse(x_train, y_train, x_test, y_test, p=False):
-    #print("within mse:", x_train.shape)
 
     x_1 = np.append(x_train, np.ones((x_train.shape[0],1)),1)
     xt = np.transpose(x_1)
 
-    #print(xt)
     betas = np.matmul(np.matmul(np.linalg.inv(np.matmul(xt, x_1)), xt), y_train)
 
     x_1_test = np.append(x_test, np.ones((x_test.shape[0],1)),1)
-    e = y_test.reshape(-1) - np.sum(x_1_test * np.transpose(betas), axis=1)
+    
+    if p:
+        y_pred = get_predicted_values(x_1_test, betas, x_train.shape[1])
+    else:
+        y_pred = np.sum(x_1_test * np.transpose(betas), axis=1)
+        
+    e = y_test.reshape(-1) - y_pred
     
     return [sum(e*e),len(x_test)]
+
+
+def get_predicted_values(x_1_test, betas, m):
+    '''
+    In the case of testing prediction performance of AR models, we want the predicted values to be generated iteratively.
+    In other words, the next predicted value results from using the previous predicted value(s), not the previous observed value(s).
+    '''
+    y_pred = x_1_test[0][:m]
+    
+    for _ in x_1_test:
+        
+        x = np.append(y_pred[:m], np.array([1]), 0)
+        pred = np.sum(x * betas)
+        y_pred = np.insert(y_pred, 0, pred)
+    
+    return np.flip(y_pred[:-m])
