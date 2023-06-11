2096c05cdbb7db0204d1dece320e2d1f46ea3e7f
mikepackard415 <72175794+mikepackard415@users.noreply.github.com>
2023-06-10 22:54:12 -0400

    Format bic.py

--- code/bic.py
+++ code/bic.py
@@ -1,9 +1,7 @@
 from mse import *
 
 #Bayesian Information Criterion
-def IC(x,y,k):
-    sq=mse(x,y,x,y)
-    s=sq[0]
-    t=sq[1]
-    bic =t*np.log(s/t)+(k+2)*np.log(t)
+def IC(x, y, k):
+    s, t = mse(x, y, x, y)
+    bic = t * np.log(s/t) + (k+2) * np.log(t)
     return [bic]
